84160bad5680348e228fe93605f1f909f7b87f14
koussay-r <koussayr98@gmail.com>
2023-10-07 22:33:23 +0100

    learning how to how to get the user input from the console.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args){
+        Scanner Sc= new Scanner(System.in);
+        System.out.println("enter your name");
+        String varriable=Sc.nextLine();
+        System.out.println("your name is "+varriable);
+        Sc.close();
         Point p=new Point();
         p.afficher();
     }
