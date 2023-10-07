582675f737a229570192f84eac15994d29b73748
koussay-r <koussayr98@gmail.com>
2023-10-08 00:21:49 +0100

    update

--- src/App.java
+++ src/App.java
@@ -1,12 +1,28 @@
-import java.util.Scanner;
 public class App {
     public static void main(String[] args){
-        Scanner Sc= new Scanner(System.in);
-        System.out.println("enter your name");
-        String varriable=Sc.nextLine();
-        System.out.println("your name is "+varriable);
-        Sc.close();
-        Point p=new Point();
-        p.afficher();
+  
+        for(int j=0;j<4;j++){
+                if(j<2){
+                    System.out.print(" ");
+                    System.out.print(" ");
+                    System.out.print(" ");
+                    System.out.println("J");
+                }
+                else if(j==2){
+                    System.out.print("J");
+                    System.out.print(" ");
+                    System.out.print(" ");
+                    System.out.println("J");
+                }
+                else{
+                    System.out.print(" ");
+                    System.out.print("J");
+                    System.out.print("J");
+                    System.out.print(" ");
+                }
+            }
+        for(int i=0;i<5;i++){
+
+        }
     }
 }
